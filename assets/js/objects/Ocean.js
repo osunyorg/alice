eb2ee0aed9928189e2d6e9478a36ebcaec4f374d
Arnaud Levy <contact@arnaudlevy.com>
2023-07-18 19:27:14 +0200

    sea

--- assets/js/objects/Ocean.js
+++ assets/js/objects/Ocean.js
@@ -4,24 +4,10 @@ import { game } from "js/Game";
 
 export default class Sea {
   constructor() {
-    // super({
-    //   width: window.innerWidth,
-    //   height: window.innerHeight,
-    //   srcWidth: 83,
-    //   srcHeight: 104,
-    //   src: '/assets/images/sea/pattern-sea.png'
-    // })
     const img = new Image()
-    img.src = '/assets/images/sea/pattern-sea.png'
+    img.src = '/assets/images/sea.gif'
     game.ctx.createPattern(img.src , 'repeat')
     game.ctx.fillStyle = ptrn;
     game.ctx.fillRect(0, 0, canvas.width, canvas.height); 
   }
-  update() {
-    // return;
-    // super.update();
-    // if (WORLD.collisions.visible)
-    //   this.drawCollisionMap();
-  }
-
 }
