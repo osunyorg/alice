fcd60655485fc4a8ea41f91100b7371e57238161
Olivia206 <olivia.simonet206@gmail.com>
2023-09-21 11:18:47 +0200

    fixed style and images

--- assets/js/game/MainGame.js
+++ assets/js/game/MainGame.js
@@ -21,8 +21,13 @@ export class Game {
     }
   }
   setup(imageExtension) {
-    this.imageExtension = imageExtension
+    this.imageExtension = imageExtension;
 
+    this.images = this.container.querySelectorAll('img');
+    this.images.forEach((image) => {
+      image.setAttribute('loading', 'lazy');
+    });
+    
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext('2d');
     this.container.appendChild(this.canvas);
