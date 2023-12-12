789ca95a1443fa992dd2fac44e3068550c4f0cb9
alexisben <alex@noesya.coop>
2023-12-12 01:07:13 +0100

    lighter

--- assets/js/game/objects/Map.js
+++ assets/js/game/objects/Map.js
@@ -9,7 +9,7 @@ export default class Map extends Thing {
       height: WORLD.height,
       srcWidth: WORLD.width * 2,
       srcHeight: WORLD.height * 2,
-      src: '/assets/images/jouer/map.png'
+      src: WORLD.mapSrc
     })
     this.setCollision();
     this.drawElements();
