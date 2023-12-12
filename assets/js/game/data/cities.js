a413a786db318a3df5c91d5632db2921a4247406
alexisben <alex@noesya.coop>
2023-12-12 18:29:56 +0100

    clean

--- assets/js/game/data/cities.js
+++ assets/js/game/data/cities.js
@@ -1,142 +1,133 @@
 
 
 export const CITIES = [
-    {
-        id: 'kryptogarten',
-        x: 80,
-        y: 660,
-        depthOffset: 40,
-        scale: 0.8,
-        sign: {
-            x: -25,
-            y: 30,
-            width: 63,
-            height: 110
-        }
-    },
-    {
-        id: 'crypto-university',
-        x: 1430,
-        y: 470,
-        scale: 0.9,
-        depthOffset: 50,
-        sign: {
-            src: "/assets/images/jouer/signs/self-made-city.png",
-            x: -50,
-            y: 20,
-            width: 80,
-            height: 115
-        }
-    },
-    {
-        id: 'la-mine-de-blocs',
-        x: 1300,
-        y: 100,
-        scale: 0.9,
-        depthOffset: 75,
-        sign: {
-            src: "/assets/images/jouer/signs/self-made-city.png",
-            x: -40,
-            y: 80,
-            width: 63,
-            height: 110
-        }
-    },
-    {
-        id: 'eglise-de-la-sainte-crypto',
-        name: 'Eglise de la sainte crypto',
-        x: 600,
-        y: 570,
-        depthOffset: 50,
-        scale: 0.75,
-        sign: {
-            src: "/assets/images/jouer/signs/self-made-city.png",
-            x: -50,
-            y: 20,
-            width: 63,
-            height: 110
-        }
-    },
-    {
-        id: 'libertaria',
-        x: 2140,
-        y: 450,
-        depthOffset: 65,
-        sign: {
-            src: "/assets/images/jouer/signs/self-made-city.png",
-            x: -50,
-            y: 20,
-            width: 63,
-            height: 110
-        }
-    },
-    {
-        id: 'le-dome-du-tonnerre',
-        x: 2100,
-        y: 180,
-        scale: 0.6,
-        depthOffset: 45,
-        sign: {
-            src: "/assets/images/jouer/signs/self-made-city.png",
-            x: -50,
-            y: 20,
-            width: 81,
-            height: 115
-        }
-    },
-    {
-        id: 'self-made-city',
-        x: 350,
-        y: 280,
-        depthOffset: 25,
-        scale: 0.75,
-        sign: {
-            src: "/assets/images/jouer/signs/self-made-city.png",
-            x: -50,
-            y: 20,
-            width: 63,
-            height: 110
-        }
-    },
-    {
-        id: 'bankochain',
-        x: 1000,
-        y: 570,
-        scale: 0.6,
-        depthOffset: 50,
-        sign: {
-            src: "/assets/images/jouer/signs/self-made-city.png",
-            x: -50,
-            y: 20,
-            width: 63,
-            height: 110
-        }
-    },
-    {
-        id: 'mordor-club',
-        x: 2160,
-        y: -60,
-        scale: 1,
-        depthOffset: 40,
-        sign: {
-            src: "/assets/images/jouer/signs/self-made-city.png",
-            x: -50,
-            y: 110,
-            width: 63,
-            height: 110
-        }
-    },
-    {
-        id: 'cimetiere-du-nft',
-        x: 1600,
-        y: 160,
-        scale: 0.8,
-        sign: {
-            src: "/assets/images/jouer/signs/self-made-city.png",
-            x: -50,
-            y: 20,
-            width: 63,
-            height: 110
-        }
+  {
+    id: 'kryptogarten',
+    x: 80,
+    y: 660,
+    depthOffset: 40,
+    scale: 0.8,
+    sign: {
+      x: -25,
+      y: 30,
+      width: 63,
+      height: 110
     }
+  },
+  {
+    id: 'crypto-university',
+    x: 1430,
+    y: 470,
+    scale: 0.9,
+    depthOffset: 50,
+    sign: {
+      x: -50,
+      y: 20,
+      width: 80,
+      height: 115
+    }
+  },
+  {
+    id: 'la-mine-de-blocs',
+    x: 1300,
+    y: 100,
+    scale: 0.9,
+    depthOffset: 75,
+    sign: {
+      x: -40,
+      y: 80,
+      width: 63,
+      height: 110
+    }
+  },
+  {
+    id: 'eglise-de-la-sainte-crypto',
+    name: 'Eglise de la sainte crypto',
+    x: 600,
+    y: 570,
+    depthOffset: 50,
+    scale: 0.75,
+    sign: {
+      x: -50,
+      y: 20,
+      width: 63,
+      height: 110
+    }
+  },
+  {
+    id: 'libertaria',
+    x: 2140,
+    y: 450,
+    depthOffset: 65,
+    sign: {
+      x: -50,
+      y: 20,
+      width: 63,
+      height: 110
+    }
+  },
+  {
+    id: 'le-dome-du-tonnerre',
+    x: 2100,
+    y: 180,
+    scale: 0.6,
+    depthOffset: 45,
+    sign: {
+      x: -50,
+      y: 20,
+      width: 81,
+      height: 115
+    }
+  },
+  {
+    id: 'self-made-city',
+    x: 350,
+    y: 280,
+    depthOffset: 25,
+    scale: 0.75,
+    sign: {
+      x: -50,
+      y: 20,
+      width: 63,
+      height: 110
+    }
+  },
+  {
+    id: 'bankochain',
+    x: 1000,
+    y: 570,
+    scale: 0.6,
+    depthOffset: 50,
+    sign: {
+      x: -50,
+      y: 20,
+      width: 63,
+      height: 110
+    }
+  },
+  {
+    id: 'mordor-club',
+    x: 2160,
+    y: -60,
+    scale: 1,
+    depthOffset: 40,
+    sign: {
+      x: -50,
+      y: 110,
+      width: 63,
+      height: 110
+    }
+  },
+  {
+    id: 'cimetiere-du-nft',
+    x: 1600,
+    y: 160,
+    scale: 0.8,
+    sign: {
+      x: -50,
+      y: 20,
+      width: 63,
+      height: 110
+    }
+  }
 ];
--- assets/js/game/data/coins.js
+++ assets/js/game/data/coins.js
@@ -1,108 +1,108 @@
 export const COINS = [
-    { 
-        id: 'bitcoin',
-        x: 300,
-        y: 800
-    },
-    {
-        id: 'blockchain',
-        x: 950,
-        y: 500
-    },
-    {
-        id: 'carte-graphique',
-        x: 1540,
-        y: 700
-    },
-    {
-        id: 'crypto-bro',
-        x: 750,
-        y: 600
-    },
-    {
-        id: 'crypto-monnaie',
-        x: 2000,
-        y: 500
-    },
-    {
-        id: 'crypto-troll',
-        x: 300,
-        y: 250
-    },
-    {
-        id: 'dao-decentralized-autonomous-organization',
-        x: 1950,
-        y: 300
-    },
-    {
-        id: 'ethereum',
-        x: 380,
-        y: 600,
-    },
-    {
-        id: 'fiat-monnaie',
-        x: 800,
-        y: 350,
-    },
-    {
-        id: 'minage',
-        x: 900,
-        y: 700,
-    },
-    {
-        id: 'minting',
-        x: 1750,
-        y: 600,
-    },
-    {
-        id: 'nft-non-fongible-token',
-        x: 1700,
-        y: 400,
-     },
-    {
-        id: 'oracle',
-        x: 1500,
-        y: 380,
-    },
-    {
-        id: 'poa-proof-of-authority',
-        x: 1300,
-        y: 500,
-    },
-    {
-        id: 'poh-proof-of-history',
-        x: 1080,
-        y: 250,
-    },
-    {
-        id: 'pos-proof-of-stake',
-        x: 2000,
-        y: 120,
-    },
-    {
-        id: 'pow-proof-of-work',
-        x: 1800,
-        y: 200,
-    },
-    {
-        id: 'satoshi-nakamoto',
-        x: 650,
-        y: 500,
-    },
-    {
-        id: 'smart-contract',
-        x: 1200,
-        y: 780,
-    },
-    {
-        id: 'stablecoin',
-        x: 150,
-        y: 550,
-    },
-    {
-        id: 'token',
-        x: 150,
-        y: 900,
-    }
+  {
+    id: 'bitcoin',
+    x: 300,
+    y: 800
+  },
+  {
+    id: 'blockchain',
+    x: 950,
+    y: 500
+  },
+  {
+    id: 'carte-graphique',
+    x: 1540,
+    y: 700
+  },
+  {
+    id: 'crypto-bro',
+    x: 750,
+    y: 600
+  },
+  {
+    id: 'crypto-monnaie',
+    x: 2000,
+    y: 500
+  },
+  {
+    id: 'crypto-troll',
+    x: 300,
+    y: 250
+  },
+  {
+    id: 'dao-decentralized-autonomous-organization',
+    x: 1950,
+    y: 300
+  },
+  {
+    id: 'ethereum',
+    x: 380,
+    y: 600,
+  },
+  {
+    id: 'fiat-monnaie',
+    x: 800,
+    y: 350,
+  },
+  {
+    id: 'minage',
+    x: 900,
+    y: 700,
+  },
+  {
+    id: 'minting',
+    x: 1750,
+    y: 600,
+  },
+  {
+    id: 'nft-non-fongible-token',
+    x: 1700,
+    y: 400,
+  },
+  {
+    id: 'oracle',
+    x: 1500,
+    y: 380,
+  },
+  {
+    id: 'poa-proof-of-authority',
+    x: 1300,
+    y: 500,
+  },
+  {
+    id: 'poh-proof-of-history',
+    x: 1080,
+    y: 250,
+  },
+  {
+    id: 'pos-proof-of-stake',
+    x: 2000,
+    y: 120,
+  },
+  {
+    id: 'pow-proof-of-work',
+    x: 1800,
+    y: 200,
+  },
+  {
+    id: 'satoshi-nakamoto',
+    x: 650,
+    y: 500,
+  },
+  {
+    id: 'smart-contract',
+    x: 1200,
+    y: 780,
+  },
+  {
+    id: 'stablecoin',
+    x: 150,
+    y: 550,
+  },
+  {
+    id: 'token',
+    x: 150,
+    y: 900,
+  }
 ];
 
